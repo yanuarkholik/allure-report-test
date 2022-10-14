58d0c59768cc2e6c8d7968ca352cdab24f1924e6
unknown <bramara.sambada.17@gmail.com>
2022-10-14 18:27:35 +0700

    Script Master Signature

---
+++ test/TC0045.js
@@ -0,0 +1,42 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+var expect = require('chai').expect;
+require('chromedriver')
+
+describe('XL Single Approval', function() {
+ 
+  before(async function() {
+    driver = await new Builder().forBrowser('chrome').build();
+    vars = {}
+  })
+  after(async function() {
+    await driver.sleep(3000);
+    await driver.quit();
+  })
+  it('Search Signature', async function() {
+    await driver.get("https://approval-fe.dev.alurkerja.com/");
+    await driver.sleep(2000);
+    await driver.manage().window().maximize();
+    await driver.sleep(2000);
+    await driver.findElement(By.xpath('//*[@id="kt_login"]/div[2]/app-login/div/div[2]/div/button[2]')).click();
+    await driver.sleep(3000);
+    // await driver.findElement(By.css("#email")).sendKeys('rahmi@javan.com');
+    // await driver.findElement(By.css("#exampleInputPassword1")).sendKeys('123456');
+    await driver.findElement(By.css("#email")).sendKeys('frezybangun@gmail.com');
+    await driver.findElement(By.css("#exampleInputPassword1")).sendKeys('12345678');
+    await driver.findElement(By.css("#kt_login > div.h-100 > app-login > div > div.col-12.col-md-6.d-flex.align-items-center.justify-content-end > div > form > button")).click();
+    await driver.sleep(2000);
+    await driver.get("https://approval-fe.dev.alurkerja.com/user");
+    await driver.sleep(7000);
+    await driver.findElement(By.css(".btn-primary:nth-child(1)")).click();
+    await driver.sleep(5000);
+    await driver.findElement(By.xpath('//*[@id="kt_body"]/ngb-modal-window/div/div/app-edit-user-modal/div/div[2]/div/form/div[3]/div[2]')).click();
+    await driver.sleep(2000);
+    await driver.findElement(By.css("#mat-option-2 > .mat-option-text")).click();
+    await driver.sleep(2000);
+    await driver.findElement(By.xpath(`//*[@id="kt_body"]/ngb-modal-window/div/div/app-edit-user-modal/div/div[2]/div/form/div[3]/div[4]/div/div/input`)).sendKeys('C:\\Users\\Brambudi\\Pictures\\javan issue\\Sample-jpg-image-50kb.jpg');
+    await driver.findElement(By.xpath(`//*[@id="kt_body"]/ngb-modal-window/div/div/app-edit-user-modal/div/div[2]/div/form/div[6]/div[2]/button`)).click();
+    await driver.sleep(7000);
+    let namaDashboard = await driver.findElement(By.xpath('//*[@id="swal2-title"]')).getText();
+    expect(namaDashboard).to.contains("Success");
+  })
+})
