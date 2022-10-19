736e927253a6d23da3b5c9e451eabe510d4da227
unknown <bramara.sambada.17@gmail.com>
2022-10-19 15:02:50 +0700

    Add env and set parameter using env

--- test/TC0042.js
+++ test/TC0042.js
@@ -13,22 +13,25 @@ describe('XL Single Approval', function() {
     await driver.quit();
   })
   it('Create Signature', async function() {
-    await driver.get("https://approval-fe.dev.alurkerja.com/");
+    let url = process.env.URL_Cloud;
+    let user = process.env.Admin_Cloud_EMAIL;
+    let pswd = process.env.Admin_Cloud_PASSWORD;
+    await driver.get(url);
     await driver.sleep(2000);
     await driver.manage().window().maximize();
     await driver.sleep(2000);
     await driver.findElement(By.xpath('//*[@id="kt_login"]/div[2]/app-login/div/div[2]/div/button[2]')).click();
     await driver.sleep(3000);
-    // await driver.findElement(By.css("#email")).sendKeys('rahmi@javan.com');
-    // await driver.findElement(By.css("#exampleInputPassword1")).sendKeys('123456');
-    await driver.findElement(By.css("#email")).sendKeys('frezybangun@gmail.com');
-    await driver.findElement(By.css("#exampleInputPassword1")).sendKeys('12345678');
+    
+    await driver.findElement(By.css("#email")).sendKeys(user);
+    await driver.findElement(By.css("#exampleInputPassword1")).sendKeys(pswd);
     await driver.findElement(By.css("#kt_login > div.h-100 > app-login > div > div.col-12.col-md-6.d-flex.align-items-center.justify-content-end > div > form > button")).click();
     await driver.sleep(2000);
-    await driver.get("https://approval-fe.dev.alurkerja.com/user");
+    // Master Signature
+    await driver.get(url+"user");
     await driver.sleep(7000);
     await driver.findElement(By.css(".btn-primary:nth-child(1)")).click();
-    await driver.sleep(5000);
+    await driver.sleep(10000);
     await driver.findElement(By.xpath('//*[@id="kt_body"]/ngb-modal-window/div/div/app-edit-user-modal/div/div[2]/div/form/div[3]/div[2]')).click();
     await driver.sleep(2000);
     await driver.findElement(By.css("#mat-option-2 > .mat-option-text")).click();
