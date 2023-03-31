2575ef7eabd15a6d3e48e1dd6c64832cad65b0f4
Widya Rizka <widyarizka88@gmail.com>
2023-03-31 21:49:38 +0700

    Master data diklat

---
+++ test/master_data/diklat/TC1409.js
@@ -0,0 +1,44 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+var expect = require('chai').expect;
+require('chromedriver')
+
+describe('MASTER DATA DIKLAT', function() {
+ 
+  before(async function() {
+    driver = await new Builder().forBrowser('chrome').build();
+    vars = {}
+    await driver.get("https://simpatik-fe.merapi.javan.id/login");
+    await driver.manage().window().maximize();
+    await driver.manage().window().setRect({ width: 1680, height: 956 });
+
+    await driver.findElement(By.css("#username")).sendKeys('doni007');
+    await driver.findElement(By.css("#password")).sendKeys('secret');
+    await driver.findElement(By.css("button[type='submit']")).click()
+    await driver.sleep(3000);   
+
+  })
+  after(async function() {
+    await driver.quit();
+  })
+
+  it('List diklat', async function() {
+    let dashboard = await driver.findElement(By.css('h1[class="font-bold text-lg my-4"]')).getText();
+    expect(dashboard).to.equal('Dashboard')
+
+    //Select Master Data on Sidebar
+    await driver.findElement(By.xpath("//*[@class='py-5 md:py-0']/nav/ul/li[4]/a/div/div")).click();
+    await driver.sleep(1000);
+    
+    //Select Menu Diklat
+    await driver.findElement(By.xpath("//*[@class='py-5 md:py-0']/nav/ul/li[4]/ul/li[5]/a")).click();
+    await driver.sleep(2000);
+
+    //Expect: There is title 'List Diklat' 
+    let list = await driver.findElement(By.css('h2[class="text-lg font-medium mr-auto flex-none"]')).getText();
+    expect(list).to.equal('List Diklat')
+
+    //Expect List
+    let list_diklat = await driver.findElement(By.xpath("//*[@class='box']/div/div/table/tbody/tr[1]/td[1]")).getText();
+    expect(list_diklat).to.equal('1');
+  })
+})
