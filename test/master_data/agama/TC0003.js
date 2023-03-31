c9842d2f3d67daea17e6f60be8d3807c480e6bb3
yanuarkholik <yanuar.0974@students.amikom.ac.id>
2023-03-31 08:26:22 +0700

    selenium simpatik

---
+++ test/master_data/agama/TC0003.js
@@ -0,0 +1,39 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+var expect = require('chai').expect;
+require('chromedriver')
+
+describe('MASTER DATA AGAMA', function() {
+ 
+  before(async function() {
+    driver = await new Builder().forBrowser('chrome').build();
+    vars = {}
+    await driver.get("https://simpatik-fe.merapi.javan.id/login");
+    await driver.manage().window().maximize();
+    await driver.manage().window().setRect({ width: 1680, height: 956 });
+  })
+  after(async function() {
+    await driver.quit();
+  })
+  it('Dapat mengubah data sub menu Agama', async function() {
+    await driver.findElement(By.css("#username")).sendKeys('doni007');
+    await driver.findElement(By.css("#password")).sendKeys('secret');
+    await driver.findElement(By.css("button[type='submit']")).click()
+    
+    // halaman list data Agama
+    await driver.wait(until.elementsLocated(By.xpath("//h1[contains(text(), 'Dashboard')]")));
+    await driver.findElement(By.linkText("Master Data")).click();
+    var ele = driver.wait(until.elementLocated(By.linkText("Agama")));
+    await ele.click();
+
+    // mencari data Agama
+    var search = "Agama yang belum ada" ;
+    var ele = driver.wait(until.elementLocated(By.name("search")));
+    await ele.sendKeys(search);
+    await ele.sendKeys(Key.ENTER);
+    await driver.wait(until.elementLocated(By.xpath("//td[contains(text(), '%d')]", search)));
+
+    // detail data Agama
+    
+
+  })
+})
---
+++ test/master_data/agama/TC0006.js
@@ -0,0 +1,36 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+var expect = require('chai').expect;
+require('chromedriver')
+
+describe('MASTER DATA AGAMA', function() {
+ 
+  before(async function() {
+    driver = await new Builder().forBrowser('chrome').build();
+    vars = {}
+    await driver.get("https://simpatik-fe.merapi.javan.id/login");
+    await driver.manage().window().maximize();
+    await driver.manage().window().setRect({ width: 1680, height: 956 });
+  })
+  after(async function() {
+    await driver.quit();
+  })
+  it('Dapat tambah data sub menu Agama lebih dari 50 karakter', async function() {
+    await driver.findElement(By.css("#username")).sendKeys('doni007');
+    await driver.findElement(By.css("#password")).sendKeys('secret');
+    await driver.findElement(By.css("button[type='submit']")).click()
+    
+    // halaman list data Agama
+    await driver.wait(until.elementsLocated(By.xpath("//h1[contains(text(), 'Dashboard')]")));
+    await driver.findElement(By.linkText("Master Data")).click();
+    var ele = driver.wait(until.elementLocated(By.linkText("Agama")));
+    await ele.click();
+
+    // tambah data agama 50 karakter
+    var agama = "Agama yang namanya lebih dari 50 karakter, coba hitung aja kalo bisa";
+    await driver.findElement(By.css(".bg-green-600")).click();
+    var ele = driver.wait(until.elementLocated(By.name("agama")));
+    await ele.sendKeys(agama);
+    await driver.findElement(By.css("button[data-btn='save']")).click();
+    await driver.wait(until.elementsLocated(By.xpath("//*[contains(text(), 'Agama maksimal berisi 50 karakter.')]")));
+  })
+})
