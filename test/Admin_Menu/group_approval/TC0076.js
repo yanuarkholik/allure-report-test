cc7c56be557ea1b870937addd0481b24ca54ddc5
yanuarkholik <yanuar.0974@students.amikom.ac.id>
2023-04-06 10:57:45 +0700

    dapat tambah data group approval

--- test/Admin_Menu/group_approval/TC0076.js
+++ test/Admin_Menu/group_approval/TC0076.js
@@ -5,39 +5,56 @@ const loginModule = require('../../login/login.js');
 describe('XL Single Approval', function() {
  
   before(async function() {
-    let user = process.env.Admin_Cloud_EMAIL;
-    let pswd = process.env.Admin_Cloud_PASSWORD;
+    let user = process.env.user3;
+    let pswd = process.env.pswd3;
     await loginModule.login(user,pswd);
   })
   after(async function() {
     await driver.sleep(3000);
     await driver.quit();
   })
-  it('Create Group Approval', async function() {
+  it('Dapat tambah data approval pada halaman Group Approval', async function() {
+  
+    /* Dapat menampilkan halaman Master Signature 
+      * Login sebagai Admin Cloud
+      * Pilih menu Admin Menu
+      * Pilih sub menu Group Approval
+      * Tampil halaman Group Approval
+      * Klik tombol Add Group Approval
+      * Isi form Group Approval
+      * Klik tombol Submit
+      * */
 
+    await driver.get("https://approval-fe.dev.alurkerja.com/group-approval");
 
-    //Group Approval
-    // await driver.get("https://approval-fe.dev.alurkerja.com/group-approval");
-    var ele = driver.wait(until.elementLocated(By.xpath("//span[contains(text(), 'Admin Menu')]")));
+    // Halaman Group Approval
+    await driver.sleep(5000);
+    var ele = driver.wait(until.elementLocated(By.xpath('//*[@id="kt_content"]/div/div/app-group-approval/div/div[1]/div[2]/button')));
     await ele.click();
-    var ele = driver.wait(until.elementLocated(By.xpath(ele + "/span[contains(text(), 'Group Approval')]")));
-    // await driver.sleep(7000);
-    // await driver.findElement(By.xpath('//*[@id="kt_content"]/div/div/app-group-approval/div/div[1]/div[2]/button')).click();
-    // await driver.sleep(3000);
-    // // Form Create Group Approval
-    // await driver.findElement(By.xpath('//*[@id="kt_body"]/ngb-modal-window/div/div/app-edit-group-approval-modal/div/div[2]/form/div[1]/div/input')).sendKeys('test Add Group Approval');
-    // // Add member in Form
-    // await driver.findElement(By.xpath('//*[@id="kt_body"]/ngb-modal-window/div/div/app-edit-group-approval-modal/div/div[2]/form/div[2]/div[1]')).click();
-    // await driver.findElement(By.xpath('//*[@id="mat-option-2"]')).click();
-    // await driver.findElement(By.xpath('//*[@id="kt_body"]/ngb-modal-window/div/div/app-edit-group-approval-modal/div/div[2]/form/div[2]/div[2]/button')).click();
-    // await driver.findElement(By.xpath('//*[@id="kt_body"]/ngb-modal-window/div/div/app-edit-group-approval-modal/div/div[2]/form/div[2]/div[1]')).click();
-    // await driver.findElement(By.xpath('//*[@id="mat-option-3"]')).click();
-    // await driver.findElement(By.xpath('//*[@id="kt_body"]/ngb-modal-window/div/div/app-edit-group-approval-modal/div/div[2]/form/div[2]/div[2]/button')).click();
-    // //Submit
-    // await driver.findElement(By.xpath('//*[@id="kt_body"]/ngb-modal-window/div/div/app-edit-group-approval-modal/div/div[3]/button')).click();
-    // await driver.sleep(8000);
-    // //Assertion
-    // let validasi = await driver.findElement(By.xpath('//*[@id="swal2-html-container"]')).getText();
-    // expect(validasi).to.contains("Success");
+    var ele = driver.wait(until.elementLocated(By.name('name')));
+    await ele.sendKeys('test Add Group Approval');
+
+    // Add member in Form
+    var member = "Ardellia Putri";
+    var member2 = "Indrianti";
+
+    await driver.findElement(By.xpath("//mat-select[@role='combobox']")).click();
+    await driver.findElement(By.xpath("//input[@aria-label='dropdown search']")).sendKeys(member);
+    var ele = driver.wait(until.elementLocated(By.xpath(`//mat-option/span[contains(text(), '${member}')]`)))
+    await ele.click();
+    await driver.findElement(By.xpath("//button[contains(text(), 'add')]")).click();
+
+    // await driver.findElement(By.xpath("//mat-select[@role='combobox']")).click();
+    // await driver.findElement(By.xpath("//input[@aria-label='dropdown search']")).sendKeys(member2);
+    // var ele = driver.wait(until.elementLocated(By.xpath(`//mat-option/span[contains(text(), '${member2}')]`)))
+    // await ele.click();
+    // await driver.findElement(By.xpath("//button[contains(text(), 'add')]")).click();
+    
+    //Submit
+    await driver.findElement(By.xpath("//button[contains(text(), 'Submit')]")).click();
+    
+    // Click button on success notification
+    var ele = driver.wait(until.elementLocated(By.xpath("//button[contains(text(), 'OK')]")));
+    await ele.click();
   })
 })
