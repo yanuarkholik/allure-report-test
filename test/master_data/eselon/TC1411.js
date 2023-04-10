ede3681ac0fe4643d98f01272ef98edbac2486a8
Widya Rizka <widyarizka88@gmail.com>
2023-04-10 08:22:04 +0700

    tambah eselon

--- test/master_data/eselon/TC1411.js
+++ test/master_data/eselon/TC1411.js
@@ -2,26 +2,36 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 var expect = require('chai').expect;
 require('chromedriver')
 
-describe('MASTER DATA AGAMA', function() {
+describe('MASTER DATA ESELON', function() {
  
   before(async function() {
     driver = await new Builder().forBrowser('chrome').build();
     vars = {}
     await driver.get("https://simpatik-fe.merapi.javan.id/login");
     await driver.manage().window().maximize();
+    await driver.manage().window().setRect({ width: 1680, height: 956 });
+
+    await driver.findElement(By.css("#username")).sendKeys('doni007');
+    await driver.findElement(By.css("#password")).sendKeys('secret');
+    await driver.findElement(By.css("button[type='submit']")).click()
+    await driver.sleep(3000);   
+
   })
   after(async function() {
     await driver.quit();
   })
-  it('Menampilkan List Eselon', async function() {
-    await driver.findElement(By.css("#username")).sendKeys('doni007');
-    await driver.findElement(By.css("#password")).sendKeys('secret');
-    await driver.findElement(By.css("button[type='submit']")).click()
+
+  it('List Eselon', async function() {
+    let dashboard = await driver.findElement(By.css('h1[class="font-bold text-lg my-4"]')).getText();
+    expect(dashboard).to.equal('Dashboard')
+
+    //Select Master Data on Sidebar
+    await driver.findElement(By.xpath("//*[@class='py-5 md:py-0']/nav/ul/li[4]/a/div/div")).click();
+    await driver.sleep(1000);
     
-    await driver.wait(until.elementsLocated(By.xpath("//h1[contains(text(), 'Dashboard')]")));
-    await driver.findElement(By.linkText("Master Data")).click();
-    var ele = driver.wait(until.elementLocated(By.linkText("Eselon")));
-    await ele.click();
+    //Select Menu Eselon
+    await driver.findElement(By.xpath("//*[@class='py-5 md:py-0']/nav/ul/li[4]/ul/li[6]/a")).click();
+    await driver.sleep(2000);
 
     //Expect: There is title 'List Eselon' 
     let list = await driver.findElement(By.css('h2[class="text-lg font-medium mr-auto flex-none"]')).getText();
