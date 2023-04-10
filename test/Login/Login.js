5ec356fb73443e498997988ca1798e4b18166ff9
yanuarkholik <yanuar.0974@students.amikom.ac.id>
2023-04-10 11:53:52 +0700

    display tanggal run tc

--- test/Login/Login.js
+++ test/Login/Login.js
@@ -8,7 +8,7 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 require('chromedriver')
 
-async function login (user,pswd, sendria)
+async function login (user,pswd)
 {
   driver = await new Builder().forBrowser('chrome').build();
   vars = {}
@@ -63,6 +63,45 @@ async function login (user,pswd, sendria)
   // await driver.wait(until.elementLocated(By.xpath('//span[contains(text(), "Home")]')));
 }
 
+async function tanggal(){
+  var date = new Date();
+  var tahun = date.getFullYear();
+  var bulan = date.getMonth();
+  var tanggal = date.getDate();
+  var hari = date.getDay();
+  var jam = date.getHours();
+  var menit = date.getMinutes();
+  var detik = date.getSeconds();
+  switch(hari) {
+  case 0: hari = "Minggu"; break;
+  case 1: hari = "Senin"; break;
+  case 2: hari = "Selasa"; break;
+  case 3: hari = "Rabu"; break;
+  case 4: hari = "Kamis"; break;
+  case 5: hari = "Jum'at"; break;
+  case 6: hari = "Sabtu"; break;
+  }
+  switch(bulan) {
+  case 0: bulan = "Januari"; break;
+  case 1: bulan = "Februari"; break;
+  case 2: bulan = "Maret"; break;
+  case 3: bulan = "April"; break;
+  case 4: bulan = "Mei"; break;
+  case 5: bulan = "Juni"; break;
+  case 6: bulan = "Juli"; break;
+  case 7: bulan = "Agustus"; break;
+  case 8: bulan = "September"; break;
+  case 9: bulan = "Oktober"; break;
+  case 10: bulan = "November"; break;
+  case 11: bulan = "Desember"; break;
+  }
+  var tampilTanggal = "Tanggal: " + hari + ", " + tanggal + " " + bulan + " " + tahun;
+  var tampilWaktu = "Jam: " + jam + ":" + menit + ":" + detik;
+  console.log(tampilTanggal);
+  console.log(tampilWaktu);
+}
+
 module.exports = {
-  login: login
+  login: login,
+  tanggal: tanggal,
 };
