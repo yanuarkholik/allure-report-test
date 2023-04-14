821162439e1642c5a414862323f4f334c45dcb9f
Bramara A. S <bramara.sambada.17@gmail.com>
2023-04-14 14:51:33 +0700

    fix Reminder Config

--- test/Admin_Menu/reminder_config/TC0100.js
+++ test/Admin_Menu/reminder_config/TC0100.js
@@ -1,27 +1,28 @@
 const { By, Key, until } = require('selenium-webdriver');
 require('chromedriver');
-const loginModule = require('../../login/login.js');
+const loginModule = require('../../Login/Login.js');
+var expect = require('chai').expect;
 
-describe('XL Single Approval', function() {
+describe('Config Reminder', function() {
  
   before(async function() {
-    let user = process.env.Admin2_Cloud_EMAIL;
-    let pswd = process.env.Admin2_Cloud_PASSWORD;
+    let user = process.env.user1;
+    let pswd = process.env.pswd1;
     await loginModule.login(user,pswd);
   })
   after(async function() {
-    await driver.sleep(3000);
     await driver.quit();
   })
-  it('Update Reminder Config', async function() {
+  it('TC0100 - Update Reminder Config', async function() {
 
     await driver.get("https://approval-fe.dev.alurkerja.com/config");
     await driver.sleep(5000);
-    await driver.findElement(By.xpath('//*[@id="example-time-input"]')).sendKeys("1000");
-    await driver.sleep(2000);
+    await driver.findElement(By.xpath('//*[@id="example-time-input"]')).sendKeys("1100");
+    // await driver.sleep(2000);
     await driver.findElement(By.xpath('//*[@id="kt_content"]/div/div/app-my-config/div/div[3]/button')).click();
     await driver.sleep(5000);
-    let validasi = await driver.findElement(By.xpath('//*[@id="swal2-title"]')).getText();
-    expect(validasi).to.contains("Success");
+    var validasi = driver.wait(until.elementLocated(By.xpath('//*[@id="swal2-title"]')));
+    var textValidasi = await validasi.getText();
+    expect(textValidasi).to.contains("Success");
   })
 })
