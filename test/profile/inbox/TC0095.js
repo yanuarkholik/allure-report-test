63cd83952ad7846733e2c17ee983bb28d61d5890
overVoid <adiferdian@outlook.com>
2023-04-20 10:04:45 +0700

    update inbox

--- test/profile/inbox/TC0095.js
+++ test/profile/inbox/TC0095.js
@@ -1,12 +1,13 @@
 const { By, Key, until } = require('selenium-webdriver');
 require('chromedriver');
+var expect = require('chai').expect;
 const loginModule = require('../../login/login.js');
 
 describe('XL Single Approval', function() {
  
   before(async function() {
-    let user = process.env.Admin2_Cloud_EMAIL;
-    let pswd = process.env.Admin2_Cloud_PASSWORD;
+    let user = process.env.user1;
+    let pswd = process.env.pswd1;
     await loginModule.login(user,pswd);
   })
   after(async function() {
@@ -17,7 +18,15 @@ describe('XL Single Approval', function() {
 
     await driver.get("https://approval-fe.dev.alurkerja.com/notification");
     await driver.sleep(5000);
-    let validasi = await driver.findElement(By.xpath('//*[@id="kt_content"]/div/div/app-my-notification/div/div[1]/div[1]/h3')).getText();
-    expect(validasi).to.contains("Notification");
+
+    await driver.wait(until.elementLocated(By.xpath("//h3[contains(text(), 'Notification')]")));
+    await driver.findElement(By.xpath("//button[contains(text(), 'Mark All as Read')]")).click();
+
+    var ele = driver.wait(until.elementLocated(By.xpath("//h2[@id='swal2-title']")));
+    var text = await ele.getText();
+    expect(text).to.equal('Success');
+    await driver.findElement(By.xpath("//button[contains(text(), 'OK')]")).click();
+
+    await driver.wait(until.elementLocated(By.xpath("//td[4]/span[contains(@class, 'xl-color') and text()='Read']")));
   })
 })
